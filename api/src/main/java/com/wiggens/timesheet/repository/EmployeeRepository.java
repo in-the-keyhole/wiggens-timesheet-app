51e807930d716eca1d1045c9674df28ff3c20fa4
dpitt <dpitt@keyholesoftware.com>
2026-05-04 14:48:21 -0500

    refresh

--- api/src/main/java/com/wiggens/timesheet/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wiggens.timesheet.repository;
-
-import com.wiggens.timesheet.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
